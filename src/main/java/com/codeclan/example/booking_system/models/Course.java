bd8dd90eecacd3a0c6f649d8acdbb44f1ffec554
Abigaila Ekierte <arekiert@gmail.com>
2019-04-11 11:03:29 +0100

    course class set up

--- src/main/java/com/codeclan/example/booking_system/models/Course.java
+++ src/main/java/com/codeclan/example/booking_system/models/Course.java
@@ -1,4 +1,62 @@
 package com.codeclan.example.booking_system.models;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "courses")
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String town;
+
+    @Column
+    private int rating;
+
+    public Course() {}
+
+    public Course(String name, String town, int rating) {
+        this.name = name;
+        this.town = town;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 }
